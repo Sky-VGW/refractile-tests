6703f833e1063055cc29c158407bb98770e68358
Sky Weiner <schuyler942@gmail.com>
2023-07-25 00:19:07 -0400

    add descriptive test naming

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-
-describe('configuration tests', () => {
+//test config 1
+describe('#empty module', () => {
   let refract;
     
   beforeAll((done) => { 
@@ -13,24 +13,21 @@ describe('configuration tests', () => {
     done();
   }); 
 
-  it('should throw a specific type of error when there is no accompanying module', () => { 
+  it('should throw a custom defined error when there is no accompanying module', () => { 
     let error; 
     let config;
 
     try { 
-      config = require(path.resolve(__dirname, '../refractile.config.js'))
-      console.log(config);            
+      config = require(path.resolve(__dirname, '../refractile.config.js'))        
       refract("nonexistent_module", "nonexistent_func")
 
     } catch (e) { 
       error = e
     }; 
+
     expect(error).not.toBe(null);
     expect(config).toStrictEqual({});
-    expect(typeof error).toBe('object');
   });
-
-
     // Remove the test config after tests are run
   afterAll(
     (done) => {
@@ -40,4 +37,65 @@ describe('configuration tests', () => {
   )
 
 });
+// test config 2
+describe('#empty config file', () => {
+  let refract;
+    
+  beforeAll((done) => { 
+    // Copy the test config to the root as the refractile.config.js
+    execSync('bash -c \"cp ./configs/config2.js ./refractile.config.js\"');
+    refract = require('refractile');
+    done();
+  }); 
 
+  it('should throw a custom defined error when there is no accompanying config file', () => { 
+    let error; 
+
+    try { 
+      refract("nonexistent_module", "nonexistent_func")
+    } catch (e) { 
+      error = e
+    }; 
+
+    expect(error).not.toBe(null);
+
+  });
+    // Remove the test config after tests are run
+  afterAll(
+    (done) => {
+      execSync('bash -c \"rm ./refractile.config.js\"');
+      done();           
+    }
+  )
+});
+//test config 3
+describe('#no bin on module', () => {
+  let refract;
+    
+  beforeAll((done) => { 
+    // Copy the test config to the root as the refractile.config.js
+    execSync('bash -c \"cp ./configs/config3.js ./refractile.config.js\"');
+    refract = require('refractile');
+    done();
+  }); 
+
+  it('should throw a custom defined error if there is no bin destination',() => {
+    let error;
+
+    try {
+      refract("nonexistent_module", "nonexistent_func");
+    } catch (e) {
+      error = e;
+      console.log(error);
+    }
+
+    expect(error).not.toBe(null);
+  });
+    // Remove the test config after tests are run
+  afterAll(
+    (done) => {
+      execSync('bash -c \"rm ./refractile.config.js\"');
+      done();           
+    }
+  )
+});
